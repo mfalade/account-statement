cc615b4c9b244ceb685d4e2825e3e98a0e036ec5
Mayowa Falade <falademayowa240@gmail.com>
2022-07-08 10:15:36 -0400

    Refactor script so only injectors are left in core file

--- account_statement/core.py
+++ account_statement/core.py
@@ -1,33 +1,8 @@
-from decimal import DivisionByZero
 import itertools
-from typing import OrderedDict
 from bs4 import BeautifulSoup
 
 import constants
 import utils
-
-
-def build_balance_sheet_row(record: OrderedDict, row_type: constants.RowType):
-    template_path = constants.template_path[row_type]
-    row_template = BeautifulSoup(open(template_path), "html.parser")
-
-    month, day = utils.get_month_and_day(record)
-    row_template.find(attrs={"name": "month"}).string.replace_with(month)
-    row_template.find(attrs={"name": "day"}).string.replace_with(day)
-    row_template.find(attrs={"name": "description"}).string.replace_with(
-        record.get("description")
-    )
-    row_template.find(attrs={"name": "debit"}).string.replace_with(
-        record.get("NEW DEBITS")
-    )
-    row_template.find(attrs={"name": "credit"}).string.replace_with(
-        record.get("NEW CREDITS")
-    )
-    row_template.find(attrs={"name": "balance"}).string.replace_with(
-        record.get("NEW BALANCE")
-    )
-
-    return row_template
 
 
 def inject_page_title(soup, group_key):
@@ -102,26 +77,22 @@ def inject_itemized_billing(soup, month_breakdown):
     return soup
 
 
-def get_empty_row(row_type: constants.RowType):
-    COLUMNS = ["month", "day", "description", "debit", "credit", "balance"]
-    template_path = constants.template_path[row_type]
-    row_template = BeautifulSoup(open(template_path), "html.parser")
-    for column in COLUMNS:
-        row_template.find(attrs={"name": column}).string.replace_with("")
+def inject_balance_sheet(soup, group):
+    balance_sheet_table = soup.find(id=constants.Selector.BALANCE_SHEET.value)
+    batch = [item for item in group]
+    for index, record in enumerate(batch):
+        row_type = (
+            constants.RowType.WHITE_ROW
+            if index % 2 == 0
+            else constants.RowType.BLACK_ROW
+        )
+        row = utils.build_balance_sheet_row(record, row_type=row_type)
+        balance_sheet_table.append(row)
 
-    return row_template
+    for extra_row in utils.get_extra_rows_for_table_padding(batch_size=len(batch)):
+        balance_sheet_table.append(extra_row)
 
-
-def get_extra_rows_for_table_padding(batch_size: int):
-    rows = [
-        get_empty_row(row_type=constants.RowType.WHITE_ROW),
-        get_empty_row(row_type=constants.RowType.BLACK_ROW),
-        get_empty_row(row_type=constants.RowType.WHITE_ROW),
-        get_empty_row(row_type=constants.RowType.BLACK_ROW),
-    ]
-    if batch_size % 2 != 0:
-        return rows[1:]
-    return rows
+    return soup
 
 
 if __name__ == "__main__":
@@ -139,25 +110,12 @@ if __name__ == "__main__":
     for index, item in enumerate(grouped_records):
         group_key, group = item
         month_breakdown = months_breakdown[index]
-        soup = BeautifulSoup(open(constants.template_path["BASE"]), "html.parser")
-
-        soup = inject_page_title(soup, group_key)
-        soup = inject_itemized_billing(soup, month_breakdown)
-
-        balance_sheet_table = soup.find(id=constants.Selector.BALANCE_SHEET.value)
         month_name = utils.get_month_name(group_key)
-        batch = [item for item in group]
-        for index, record in enumerate(batch):
-            row_type = (
-                constants.RowType.WHITE_ROW
-                if index % 2 == 0
-                else constants.RowType.BLACK_ROW
-            )
-            row = build_balance_sheet_row(record, row_type=row_type)
-            balance_sheet_table.append(row)
 
-        for extra_row in get_extra_rows_for_table_padding(batch_size=len(batch)):
-            balance_sheet_table.append(extra_row)
+        soup = BeautifulSoup(open(constants.template_path["BASE"]), "html.parser")
+        soup = inject_page_title(soup, group_key)
+        soup = inject_balance_sheet(soup, group)
+        soup = inject_itemized_billing(soup, month_breakdown)
 
         utils.write_to_file(
             file_path=f"results/{month_name}.html", content=soup.prettify()
--- account_statement/utils.py
+++ account_statement/utils.py
@@ -1,6 +1,9 @@
 import csv
 from datetime import datetime
 from typing import List, OrderedDict, Tuple
+from bs4 import BeautifulSoup
+
+import constants
 
 
 def read_file_content(file_path: str, delimiter: str = "") -> List[OrderedDict]:
@@ -22,3 +25,48 @@ def write_to_file(file_path: str, content: str) -> None:
 
 def get_month_name(month_digit: str, output_format: str = "%B") -> str:
     return datetime.strptime(str(month_digit), "%m").strftime(output_format)
+
+
+def get_empty_row(row_type: constants.RowType):
+    COLUMNS = ["month", "day", "description", "debit", "credit", "balance"]
+    template_path = constants.template_path[row_type]
+    row_template = BeautifulSoup(open(template_path), "html.parser")
+    for column in COLUMNS:
+        row_template.find(attrs={"name": column}).string.replace_with("")
+
+    return row_template
+
+
+def get_extra_rows_for_table_padding(batch_size: int):
+    rows = [
+        get_empty_row(row_type=constants.RowType.WHITE_ROW),
+        get_empty_row(row_type=constants.RowType.BLACK_ROW),
+        get_empty_row(row_type=constants.RowType.WHITE_ROW),
+        get_empty_row(row_type=constants.RowType.BLACK_ROW),
+    ]
+    if batch_size % 2 != 0:
+        return rows[1:]
+    return rows
+
+
+def build_balance_sheet_row(record: OrderedDict, row_type: constants.RowType):
+    template_path = constants.template_path[row_type]
+    row_template = BeautifulSoup(open(template_path), "html.parser")
+
+    month, day = get_month_and_day(record)
+    row_template.find(attrs={"name": "month"}).string.replace_with(month)
+    row_template.find(attrs={"name": "day"}).string.replace_with(day)
+    row_template.find(attrs={"name": "description"}).string.replace_with(
+        record.get("description")
+    )
+    row_template.find(attrs={"name": "debit"}).string.replace_with(
+        record.get("NEW DEBITS")
+    )
+    row_template.find(attrs={"name": "credit"}).string.replace_with(
+        record.get("NEW CREDITS")
+    )
+    row_template.find(attrs={"name": "balance"}).string.replace_with(
+        record.get("NEW BALANCE")
+    )
+
+    return row_template
